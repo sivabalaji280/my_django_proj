98ad0e7eeec5612d407340e17a36f9ad096d5394
balu <balaji@gamil.com>
2023-10-23 09:08:12 +0530

    my django proj

---
+++ accounts/views.py
@@ -0,0 +1,19 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request,'home.html',{"name":"balaji"})
+    # return HttpResponse("hello... how are you ")
+
+def add(request):
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    result = val1 + val2
+    return render(request,'result.html',{'result': result})
+
+
+
+ 
